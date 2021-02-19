794bb04b2ab8da4482ff2eafe0418401f0964e62
Raul Salazar <raulsalazarpacheco@gmail.com>
2021-02-18 22:38:34 -0500

    added peek

--- src/main/java/plc/project/Parser.java
+++ src/main/java/plc/project/Parser.java
@@ -163,7 +163,26 @@ public final class Parser {
      * {@code peek(Token.Type.IDENTIFIER)} and {@code peek("literal")}.
      */
     private boolean peek(Object... patterns) {
-        throw new UnsupportedOperationException(); //TODO (in lecture)
+        //throw new UnsupportedOperationException(); //TODO (in lecture)
+        for(int i = 0; i < patterns.length; i++){
+            if(!tokens.has(i)){
+                return false;
+            }
+            else if(patterns[i] instanceof Token.Type){
+                if(patterns[i] != tokens.get(i).getType()){
+                    return false;
+                }
+            }
+            else if(patterns[i] instanceof String){
+                if(!patterns[i].equals(tokens.get(i).getLiteral())){
+                    return false;
+                }
+            }
+            else{
+                throw new AssertionError("Invalid pattern object: " + patterns[i].getClass());
+            }
+        }
+        return true;
     }
 
     /**
