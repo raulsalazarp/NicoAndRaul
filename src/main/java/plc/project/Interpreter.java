56bd614d0c67909d645ab265685d56fd1e75af1a
Raul Salazar <raulsalazarpacheco@gmail.com>
2021-03-11 15:30:55 -0500

    new test file

--- src/main/java/plc/project/Interpreter.java
+++ src/main/java/plc/project/Interpreter.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.math.RoundingMode;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -66,7 +67,7 @@ public class Interpreter implements Ast.Visitor<Environment.PlcObject> {
                 visit(((Ast.Expr.Access) ast.getReceiver()).getReceiver().get()).setField(ast.getReceiver().toString(),visit(ast.getValue()));
             }
             else{
-                scope.lookupVariable(ast.getReceiver().toString()).setValue(visit(ast.getValue()));
+                scope.lookupVariable(((Ast.Expr.Access) ast.getReceiver()).getName()).setValue(visit(ast.getValue()));
             }
         }
         return Environment.NIL;
@@ -74,7 +75,6 @@ public class Interpreter implements Ast.Visitor<Environment.PlcObject> {
 
     @Override
     public Environment.PlcObject visit(Ast.Stmt.If ast) {
-        //TODO
         if(requireType(Boolean.class, visit(ast.getCondition()))){
             try{
                 scope = new Scope(scope);
@@ -98,14 +98,11 @@ public class Interpreter implements Ast.Visitor<Environment.PlcObject> {
             }
         }
         return Environment.NIL;
-
     }
 
     @Override
     public Environment.PlcObject visit(Ast.Stmt.For ast) {
-
         Iterable<Environment.PlcObject> ting = requireType(Iterable.class , visit(ast.getValue()));
-
         for(Environment.PlcObject x : ting){
             try{
                 scope = new Scope(scope);
@@ -164,12 +161,166 @@ public class Interpreter implements Ast.Visitor<Environment.PlcObject> {
 
     @Override
     public Environment.PlcObject visit(Ast.Expr.Binary ast) {
+        //AND or OR
         if(ast.getOperator().equals("AND") || ast.getOperator().equals("OR")){
             if(requireType(Boolean.class, visit(ast.getLeft()))){
                 if(ast.getOperator().equals("OR")){
-
+                    return new Environment.PlcObject(scope, Boolean.TRUE);
+                    //return boolean value wrapped around a plc object
+                }
+                else if(ast.getOperator().equals("AND")){
+                    if(requireType(Boolean.class, visit(ast.getRight()))){
+                        return new Environment.PlcObject(scope, Boolean.TRUE);
+                    }
                 }
             }
+            else{
+                return new Environment.PlcObject(scope, Boolean.FALSE);
+            }
+        }
+        //comparisons
+        else if(ast.getOperator().equals("<") || ast.getOperator().equals("<=") || ast.getOperator().equals(">") || ast.getOperator().equals(">=")){
+            if((Comparable.class.isInstance((ast.getLeft()))) && (Comparable.class.isInstance((ast.getRight())))){
+                int compres = (((Comparable<Ast.Expr>)(ast.getLeft())).compareTo((ast.getRight()))); //compres = comparison result
+                if(ast.getOperator().equals("<")){
+                    if(compres == -1){ //right greater than left
+                        //if confused about return type look at the interpreter tests
+                        return new Environment.PlcObject(scope, Boolean.TRUE);
+                    }
+                    else {
+                        return new Environment.PlcObject(scope, Boolean.FALSE);
+                    }
+                }
+                else if(ast.getOperator().equals("<=")){
+                    if(compres == -1 || compres == 0){ //right greater than left
+                        return new Environment.PlcObject(scope, Boolean.TRUE);
+                    }
+                    else{
+                        return new Environment.PlcObject(scope, Boolean.FALSE);
+                    }
+                }
+                else if(ast.getOperator().equals(">")){
+                    if(compres == 1){
+                        return new Environment.PlcObject(scope, Boolean.TRUE);
+                    }
+                    else{
+                        return new Environment.PlcObject(scope, Boolean.FALSE);
+                    }
+                }
+                else if(ast.getOperator().equals(">=")){
+                    if(compres == 0 || compres == 1){
+                        return new Environment.PlcObject(scope, Boolean.TRUE);
+                    }
+                    else{
+                        return new Environment.PlcObject(scope, Boolean.FALSE);
+                    }
+                }
+            }
+            else{
+                throw new RuntimeException("Error: Left and/or Right are not comparable");
+            }
+        }
+        //equalities
+        else if(ast.getOperator().equals("==") || ast.getOperator().equals("!=")){
+            if(ast.getOperator().equals("==")){
+                if(ast.getLeft().equals(ast.getRight())){
+                    return new Environment.PlcObject(scope, Boolean.TRUE);
+                }
+                else{
+                    return new Environment.PlcObject(scope, Boolean.FALSE);
+                }
+            }
+            else if(ast.getOperator().equals("!=")){
+                if(ast.getLeft().equals(ast.getRight()) == false){
+                    return new Environment.PlcObject(scope, Boolean.TRUE);
+                }
+                else{
+                    return new Environment.PlcObject(scope, Boolean.FALSE);
+                }
+            }
+        }
+        //the plus sign
+        else if(ast.getOperator().equals("+")){
+//            String left = "";
+//            String right = "";
+            if(ast.getLeft() instanceof Ast.Expr.Access && ast.getRight() instanceof Ast.Expr.Access){
+//                left = ((Ast.Expr.Access) ast.getLeft()).getName();
+//                right = ((Ast.Expr.Access) ast.getRight()).getName();
+//                String x = left+right;
+//                return new Environment.PlcObject(scope, x);
+                Environment.Variable left = scope.lookupVariable(((Ast.Expr.Access) ast.getLeft()).getName());
+                Environment.Variable right = scope.lookupVariable(((Ast.Expr.Access) ast.getRight()).getName());
+
+            }
+            else{
+                //if either is a string but not access
+                if(((Ast.Expr.Literal)ast.getLeft()).getLiteral() instanceof String || ((Ast.Expr.Literal)ast.getRight()).getLiteral() instanceof String ){
+                    //concatenate the strings
+                    String x = ((String)(((Ast.Expr.Literal)ast.getLeft()).getLiteral()))+((String)(((Ast.Expr.Literal)ast.getRight()).getLiteral()));
+                    return new Environment.PlcObject(scope, x);
+                }
+                else if(((Ast.Expr.Literal)ast.getLeft()).getLiteral() instanceof BigInteger && ((Ast.Expr.Literal)ast.getRight()).getLiteral() instanceof BigInteger){
+                    BigInteger x = ((BigInteger)(((Ast.Expr.Literal)ast.getLeft()).getLiteral())).add(((BigInteger)(((Ast.Expr.Literal)ast.getRight()).getLiteral())));
+                    return new Environment.PlcObject(scope,x);
+                }
+                else if(((Ast.Expr.Literal)ast.getLeft()).getLiteral() instanceof BigDecimal && ((((Ast.Expr.Literal)ast.getRight()).getLiteral())) instanceof BigDecimal){
+                    BigDecimal x = ((BigDecimal)(((Ast.Expr.Literal)ast.getLeft()).getLiteral())).add(((BigDecimal)(((Ast.Expr.Literal)ast.getRight()).getLiteral())));
+                    return new Environment.PlcObject(scope,x);
+                }
+            }
+            throw new RuntimeException("Error: the BigInteger/BigDecimal types are mismatched for ast.left and ast.right");
+        }
+        else if(ast.getOperator().equals("-") || ast.getOperator().equals("*")){
+            if(ast.getOperator().equals("-")){
+                if(((Ast.Expr.Literal)ast.getLeft()).getLiteral() instanceof BigInteger && ((Ast.Expr.Literal)ast.getRight()).getLiteral() instanceof BigInteger){
+                    BigInteger x = ((BigInteger)(((Ast.Expr.Literal)ast.getLeft()).getLiteral())).subtract(((BigInteger)(((Ast.Expr.Literal)ast.getRight()).getLiteral())));
+                    return new Environment.PlcObject(scope,x);
+                }
+                else if(((Ast.Expr.Literal)ast.getLeft()).getLiteral() instanceof BigDecimal && ((((Ast.Expr.Literal)ast.getRight()).getLiteral())) instanceof BigDecimal){
+                    BigDecimal x = ((BigDecimal)(((Ast.Expr.Literal)ast.getLeft()).getLiteral())).subtract(((BigDecimal)(((Ast.Expr.Literal)ast.getRight()).getLiteral())));
+                    return new Environment.PlcObject(scope,x);
+                }
+                else{
+                    throw new RuntimeException("Error: the BigInteger/BigDecimal types are mismatched for ast.left and ast.right");
+                }
+            }
+            else{
+                if(((Ast.Expr.Literal)ast.getLeft()).getLiteral() instanceof BigInteger && ((Ast.Expr.Literal)ast.getRight()).getLiteral() instanceof BigInteger){
+                    BigInteger x = ((BigInteger)(((Ast.Expr.Literal)ast.getLeft()).getLiteral())).multiply(((BigInteger)(((Ast.Expr.Literal)ast.getRight()).getLiteral())));
+                    return new Environment.PlcObject(scope,x);
+                }
+                else if(((Ast.Expr.Literal)ast.getLeft()).getLiteral() instanceof BigDecimal && ((((Ast.Expr.Literal)ast.getRight()).getLiteral())) instanceof BigDecimal){
+                    BigDecimal x = ((BigDecimal)(((Ast.Expr.Literal)ast.getLeft()).getLiteral())).multiply(((BigDecimal)(((Ast.Expr.Literal)ast.getRight()).getLiteral())));
+                    return new Environment.PlcObject(scope,x);
+                }
+                else{
+                    throw new RuntimeException("Error: the BigInteger/BigDecimal types are mismatched for ast.left and ast.right");
+                }
+            }
+        }
+        else if(ast.getOperator().equals("/")){
+            if(((Ast.Expr.Literal)ast.getRight()).getLiteral() instanceof BigInteger && ((BigInteger)(((Ast.Expr.Literal)ast.getRight()).getLiteral())).compareTo(BigInteger.ZERO) == 0){
+                throw new RuntimeException("Error: denominator is zero");
+            }
+            if(((Ast.Expr.Literal)ast.getRight()).getLiteral() instanceof BigDecimal && ((BigDecimal)(((Ast.Expr.Literal)ast.getRight()).getLiteral())).compareTo(BigDecimal.ZERO) == 0){
+                throw new RuntimeException("Error: denominator is zero");
+            }
+            if(((Ast.Expr.Literal)ast.getLeft()).getLiteral() instanceof BigInteger && ((Ast.Expr.Literal)ast.getRight()).getLiteral() instanceof BigInteger){
+                BigInteger x = ((BigInteger)(((Ast.Expr.Literal)ast.getLeft()).getLiteral())).divide(((BigInteger)(((Ast.Expr.Literal)ast.getRight()).getLiteral())));
+                return new Environment.PlcObject(scope,x);
+            }
+            else if(((Ast.Expr.Literal)ast.getLeft()).getLiteral() instanceof BigDecimal && ((((Ast.Expr.Literal)ast.getRight()).getLiteral())) instanceof BigDecimal){
+                BigDecimal x = ((BigDecimal)(((Ast.Expr.Literal)ast.getLeft()).getLiteral())).divide(((BigDecimal)(((Ast.Expr.Literal)ast.getRight()).getLiteral())),1,BigDecimal.ROUND_HALF_EVEN);
+                //is two the correct number for this?? ask in OH
+                return new Environment.PlcObject(scope,x);
+            }
+            else{
+                throw new RuntimeException("Error: the BigInteger/BigDecimal types are mismatched for ast.left and ast.right");
+            }
+        }
+        else{
+            throw new RuntimeException("Error: something went wrong in binary method");
+            //this is how to throw errords
         }
         return Environment.NIL;
     }
