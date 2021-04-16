b2ff53486d7f3bb7663d981199a1180c54be0755
Raul Salazar <raulsalazarpacheco@gmail.com>
2021-04-16 18:01:16 -0400

    ready for final submission

--- src/main/java/plc/project/Generator.java
+++ src/main/java/plc/project/Generator.java
@@ -81,10 +81,6 @@ public final class Generator implements Ast.Visitor<Void> {
         return null;
     }
 
-    public void methodHelper(){
-
-        return;
-    }
     @Override
     public Void visit(Ast.Method ast) {
 
@@ -92,23 +88,25 @@ public final class Generator implements Ast.Visitor<Void> {
 
         String inner = "";
         for(int i = 0; i < ast.getParameters().size(); i++){
+            inner = inner + ast.getFunction().getParameterTypes().get(i).getJvmName() +" "+ ast.getParameters().get(i);
             if(i < ast.getParameters().size()-1){
-                //convert parameter type names to jvm names
-                inner = inner + ast.getParameterTypeNames().get(i) +" "+ ast.getParameters().get(i) + ", ";
-            }
-            else{
-                inner = inner + ast.getParameterTypeNames().get(i) +" "+ ast.getParameters().get(i);
+                inner = inner + ", ";
             }
         }
         print(ast.getFunction().getReturnType().getJvmName()," ",ast.getFunction().getJvmName(),"(",inner,") {");
-        indent++;
-        for(Ast.Stmt x : ast.getStatements()){
-            newline(indent);
-            print((x));
+        if(ast.getStatements().size() == 0){
+            print("}");
         }
-        indent--;
-        newline(indent);
-        print("}");
+        else{
+            indent++;
+            for(Ast.Stmt x : ast.getStatements()){
+                newline(indent);
+                print((x));
+            }
+            indent--;
+            newline(indent);
+            print("}");
+        }
 
         return null;
     }
@@ -143,19 +141,21 @@ public final class Generator implements Ast.Visitor<Void> {
     @Override
     public Void visit(Ast.Stmt.If ast) {
         print("if (",(ast.getCondition()),") {");
-        indent++;
-        for(Ast.Stmt x : ast.getThenStatements()){
+        if(ast.getThenStatements().size() > 0){
+            indent++;
+            for(Ast.Stmt x : ast.getThenStatements()){
+                newline(indent);
+                print((x));
+            }
+            indent--;
             newline(indent);
-            print((x));
-        }
-        indent--;
-        newline(indent);
-        if(ast.getElseStatements().size() < 1){
             print("}");
-
         }
         else{
-            print("} else {");
+            print("}");
+        }
+        if(ast.getElseStatements().size() > 0){
+            print(" else {");
             indent++;
             for(Ast.Stmt x : ast.getElseStatements()){
                 newline(indent);
@@ -164,62 +164,41 @@ public final class Generator implements Ast.Visitor<Void> {
             indent--;
             newline(indent);
             print("}");
-
         }
 
         return null;
     }
 
-    public String t2s(Environment.Type x){//strint to type
-        if(x.equals(Environment.Type.ANY))
-            return "Any";
-        if(x.equals(Environment.Type.NIL))
-            return "Nil";
-        if(x.equals(Environment.Type.INTEGER_ITERABLE))
-            return "IntegerIterable";
-        if(x.equals(Environment.Type.COMPARABLE))
-            return "Comparable";
-        if(x.equals(Environment.Type.BOOLEAN))
-            return "boolean";
-        if(x.equals(Environment.Type.INTEGER))
-            return "int";
-        if(x.equals(Environment.Type.DECIMAL))
-            return "double";
-        if(x.equals(Environment.Type.CHARACTER))
-            return "char";
-        if(x.equals(Environment.Type.STRING))
-            return "String";
-        throw new RuntimeException("Something went wrong with s2t");
-    }
-
     @Override
-    public Void visit(Ast.Stmt.For ast) { //estoy perdidisimo
-        //would value.getType be LIST or the TYPE OF THE LIST
-        Environment.Type vartype = ast.getValue().getType();
-        String type = t2s(vartype);
+    public Void visit(Ast.Stmt.For ast) {
+        String type = ast.getValue().getType().getJvmName();
         print("for (",type," ",ast.getName()," : ",(ast.getValue()),") {");
-        indent++;
-        for(Ast.Stmt x : ast.getStatements()){
+        if(ast.getStatements().size() > 0){
+            indent++;
+            for(Ast.Stmt x : ast.getStatements()){
+                newline(indent);
+                print((x));
+            }
+            indent--;
             newline(indent);
-            print((x));
         }
-        indent--;
-        newline(indent);
         print("}");
-
         return null;
     }
 
     @Override
     public Void visit(Ast.Stmt.While ast) {
+        //special case of statements being empty
         print("while (",(ast.getCondition()),") {");
-        indent++;
-        for(Ast.Stmt x : ast.getStatements()){
+        if(ast.getStatements().size() > 0){
+            indent++;
+            for(Ast.Stmt x : ast.getStatements()){
+                newline(indent);
+                print((x));
+            }
+            indent--;
             newline(indent);
-            print((x));
         }
-        indent--;
-        newline(indent);
         print("}");
 
         return null;
@@ -267,43 +246,14 @@ public final class Generator implements Ast.Visitor<Void> {
         Ast.Expr left = ast.getLeft();
         Ast.Expr right = ast.getRight();
 
-        /** NOT VISITING **/ //collapsed under this line
-
         if(op.equals("AND")){
-            print(left," && ",right);
+            print(left, " ", "&&", " ", right);
         }
         else if(op.equals("OR")){
-            print(left,"||",right);
+            print(left, " ", "||", " ", right);
         }
-        else if(op.equals("<")){
-            print(left,"<",right);
-        }
-        else if(op.equals(">")){
-            print(left,">",right);
-        }
-        else if(op.equals("<=")){
-            print(left,"<=",right);
-        }
-        else if(op.equals(">=")){
-            print(left,">=",right);
-        }
-        else if(op.equals("==")){
-            print(left,"==",right);
-        }
-        else if(op.equals("!=")){
-            print(left,"!=",right);
-        }
-        else if(op.equals("+")){
-            print(left," + ",right);
-        }
-        else if(op.equals("-")){
-            print((left)," - ",(right));
-        }
-        else if(op.equals("*")){
-            print((left)," * ",(right));
-        }
-        else if(op.equals("/")){
-            print((left)," / ",(right));
+        else{
+            print(left, " ", op, " ", right);
         }
 
         return null;
@@ -313,60 +263,31 @@ public final class Generator implements Ast.Visitor<Void> {
     public Void visit(Ast.Expr.Access ast) {
         String name = ast.getVariable().getJvmName();
         if(ast.getReceiver().isPresent()){
-            print((ast.getReceiver().get()),".",(name));
+            print((ast.getReceiver().get()),".");
         }
-        else{
-            print(name);
-        }
+        print(name);
         return null;
     }
 
-    public void functionHelper(List<Ast.Expr> list){
-        for(int i = 0; i < list.size(); i++){
-            if(i < list.size()-1){
-                visit(list.get(i));
-                print(", ");
-            }
-            else{
-                visit(list.get(i));
-            }
-        }
-        return;
-    }
     @Override
     public Void visit(Ast.Expr.Function ast) {
         String name = ast.getFunction().getJvmName();
-
         if(ast.getReceiver().isPresent()){
-            print((ast.getReceiver().get()),".",name,"(");
-            functionHelper(ast.getArguments());
-            print(")");
+            print((ast.getReceiver().get()),".");
         }
-        else{
-            print(name,"(");
-            functionHelper(ast.getArguments());
-            print(")");
+        print(name,"(");
+        if(ast.getArguments().size() > 0){
+            for(int i = 0; i < ast.getArguments().size(); i++){
+                print(ast.getArguments().get(i));
+                if(i < ast.getArguments().size()-1){
+                    print(", ");
+                }
+            }
         }
+        print(")");
         return null;
     }
-
 }
-/*
-GeneratorTests (9/17):
-  Source (1/2):
-    Multiple Fields & Methods: Incorrect result, received public class Main {␍␊    Integer x;␍␊    Decimal y;␍␊    String z;␍␊␍␊    public static void main(String[] args) {␍␊        System.exit(new Main().main());␍␊    }␍␊␍␊    int f() {␍␊        return x;␍␊    }␍␊    double g() {␍␊        return y;␍␊    }␍␊    String h() {␍␊        return z;␍␊    }␍␊    int main() {␍␊    }␍␊␍␊}
-  Field (0/2):
-    Declaration: Incorrect result, received Integer name;
-    Initialization: Incorrect result, received Decimal name = 1.0;
-  Method (0/2):
-    Square: Incorrect result, received double square(Decimal num) {␍␊    return num * num;␍␊}
-    Multiple Statements: Incorrect result, received Void func(Integer x, Decimal y, String z) {␍␊    System.out.println(x);␍␊    System.out.println(y);␍␊    System.out.println(z);␍␊}
-  Stmt (4/6):
-    For (0/1):
-      For: Incorrect result, received for (IntegerIterable num : list) {␍␊    System.out.println(num);␍␊}
-    While (1/2):
-      Empty Statements: Incorrect result, received while (cond) {␍␊}
-  Expr (4/5):
-    Binary (3/4):
-      Comparison: Incorrect result, received 1>10
- */
+
+//ready for final submission
+
